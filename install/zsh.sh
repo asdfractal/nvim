37f65fab999c37902471bab9948779f689809dd3
James Gregory <james@asdfractal.com>
2023-04-29 18:06:14 +1000

    Add finished message and change shell to zsh

--- install/zsh.sh
+++ install/zsh.sh
@@ -6,3 +6,4 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+command chsh -s $(which zsh)
