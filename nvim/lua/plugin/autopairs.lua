99c2d4d968a513db4f257f74efe9e6e1134e3496
James Gregory <james@asdfractal.com>
2024-08-12 23:34:20 +1000

    autopairs

---
+++ nvim/lua/plugin/autopairs.lua
@@ -0,0 +1,59 @@
+return {
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        -- Optional dependency
+        dependencies = { "hrsh7th/nvim-cmp" },
+        config = function()
+            require("nvim-autopairs").setup({
+                enable_moveright = true,
+            })
+            -- If you want to automatically add `(` after selecting a function or method
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            local cmp = require("cmp")
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+    },
+    -- Lua
+    {
+        "abecodes/tabout.nvim",
+        lazy = false,
+        config = function()
+            require("tabout").setup({
+                tabkey = "<BS>", -- key to trigger tabout, set to an empty string to disable
+                backwards_tabkey = "<BS>", -- key to trigger backwards tabout, set to an empty string to disable
+                act_as_tab = false, -- shift content if tab out is not possible
+                act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+                default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+                default_shift_tab = "<C-d>", -- reverse shift default action,
+                enable_backwards = true, -- well ...
+                completion = false, -- if the tabkey is used in a completion pum
+                tabouts = {
+                    { open = "'", close = "'" },
+                    { open = '"', close = '"' },
+                    { open = "`", close = "`" },
+                    { open = "(", close = ")" },
+                    { open = "[", close = "]" },
+                    { open = "{", close = "}" },
+                },
+                ignore_beginning = false, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+                exclude = {}, -- tabout will ignore these filetypes
+            })
+        end,
+        dependencies = { -- These are optional
+            "nvim-treesitter/nvim-treesitter",
+            "L3MON4D3/LuaSnip",
+            "hrsh7th/nvim-cmp",
+        },
+        opt = false, -- Set this to true if the plugin is optional
+        event = "InsertCharPre", -- Set the event to 'InsertCharPre' for better compatibility
+        priority = 1000,
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        keys = function()
+            -- Disable default tab keybinding in LuaSnip
+            return {}
+        end,
+    },
+}
