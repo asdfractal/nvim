014503e2c27a467c978cf980e1a0b954790bd430
James Gregory <james@asdfractal.com>
2024-08-16 19:43:37 +1000

    disable missing fields lua

--- nvim/lua/plugin/lsp.lua
+++ nvim/lua/plugin/lsp.lua
@@ -50,9 +50,9 @@ return {
                             },
                             diagnostics = {
                                 globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                                disable = { 'missing-fields' },
                             },
                             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                            -- diagnostics = { disable = { 'missing-fields' } },
                             workspace = {
                                 checkThirdParty = false,
                                 -- library = {
